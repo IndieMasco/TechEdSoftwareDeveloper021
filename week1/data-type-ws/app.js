413c9226a543add70d287fce2bc355e931421ce4
sam <s.clarkyy.1994@gmail.com>
2025-08-15 12:23:56 +0100

    just pushing the work i have done before lunch

---
+++ week1/data-type-ws/app.js
@@ -0,0 +1,5 @@
+let MyfavouriteGameIs = "borderlands";
+let AtTheMomentIveGot = "200";
+
+const sentence = `My favourite game is ${MyfavouriteGameIs}. at the moment ive got ${AtTheMomentIveGot} hours logged on the game.`;
+console.log(sentence);
