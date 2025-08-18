f90db7840cbc665defdc58c7e596e68cc0f17eaf
sam <s.clarkyy.1994@gmail.com>
2025-08-18 10:47:19 +0100

    pushing finished workshop

---
+++ week2/arrays-ws/app.js
@@ -0,0 +1,23 @@
+console.log("Arrays Workshop");
+
+const blogPosts = [
+  "My first blog post",
+  "My second blog post",
+  "My third blog post",
+];
+
+console.log(blogPosts[0]);
+console.log(blogPosts[1]);
+console.log(blogPosts[2]);
+
+blogPosts[3] = "My fourth blog post";
+
+console.log(blogPosts[3]);
+
+const favouriteNumbers = [7, 42, 3.14, 2.71828];
+const favouriteFoods = ["pizza", "sushi", "ice cream", "tacos"];
+const favouriteColours = ["blue", "green", "red", "purple"];
+
+const randomList = [42, "pizza", "purple"];
+
+console.log(randomList);
