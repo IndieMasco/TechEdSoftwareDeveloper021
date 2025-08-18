121d5cb991c767ac7993ea9c7606f203576be60d
sam <s.clarkyy.1994@gmail.com>
2025-08-18 10:13:09 +0100

    push before break

---
+++ week2/arrays/app.js
@@ -0,0 +1,56 @@
+// storing out array in a variable
+
+let myArray = ["item1", "item2", "item3"];
+
+//exaples
+// an array can have multiple data types stored in it
+let TecgEdStaff = ["Seth", "Chris", "John"];
+let MyFavoriteFoods = ["Pizza", 10, null, true];
+
+//length of an array
+// the length of an array is the number of items in it
+// techEdStaff.length; // 3
+// MyFavoriteFoods.length; // 4
+
+// we can access the length property of an array
+TechEdStaff.length; // 3
+
+console.log(TechEdStaff.length); // 3
+console.log(MyFavoriteFoods.length); // 4
+
+// we can also use the length property to loop through an array
+for (let i = 0; i < TechEdStaff.length; i++) {
+  console.log(TechEdStaff[i]);
+}
+
+// we can access the items in an array using their index
+// index starts at 0
+// TechEdStaff[0]; // "Seth"
+// TechEdStaff[1]; // "Chris"
+// TechEdStaff[2]; // "John"
+
+// we can also use the length property to access the last item in an array
+// TechEdStaff[TechEdStaff.length - 1]; // "John"
+
+// Index
+// INdex is a valur (number) that represents the position of an item in an array
+// The first item in an array has an index of 0
+// The second item in an array has an index of 1
+// The third item in an array has an index of 2
+// and so on...
+
+TechEdStaff[0]; // "Seth"
+TechEdStaff[1]; // "Chris"
+TechEdStaff[2]; // "John"
+
+console.log(TechEdStaff[0]); // "Seth"
+
+// what is the index of the last item in an array?
+// we can use the length property to find the index of the last item in an array
+TechEdStaff(TechEdStaff.length - 1); // "John"
+console.log(TechEdStaff[TechEdStaff.length - 1]); // "John"
+
+// we can also use the length property to loop through an array backwards
+for (let i = TechEdStaff.length - 1; i >= 0; i--) {
+  console.log(TechEdStaff[i]);
+}
