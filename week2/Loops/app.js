4019719b3e119ea5979846c1871a62f7203c568d
sam <s.clarkyy.1994@gmail.com>
2025-08-19 10:28:20 +0100

    break time push

--- week2/Loops/app.js
+++ week2/Loops/app.js
@@ -6,7 +6,7 @@ console.log("This is the app.js file in the Loops directory of week2.");
 // the task to be executed
 
 // for (initialisation; condition; afterthought) {
-//     // action
+// action
 // }
 // initialisation: this is where we set up the loop, we can set a variable to start at 0 or 1
 // condition: this is where we set the condition for the loop to continue, for example, i want to loop until i reach 5
@@ -43,3 +43,46 @@ for (let i = 0; i < subjects.length; i++) {
   console.log(subjects[i]);
 }
 // the initial value [i] is also reprenting the index number
+
+// simplified for loops
+// these for loops work in the same way but they syntax is simpler
+
+// for ... of
+// let subject --> this represents each item in the array
+// of subjects --> this is the condition (length is included)
+// the afterthought is implied
+for (let subject of subjects) {
+  console.log(subject);
+}
+
+// forEach()
+// a methed --> a JS default function
+// structure
+subjects.forEach(function (subject) {
+  console.log(subject);
+});
+
+//TODO: i want to render <display, show) 3 images in my page
+// - i need a container to hold the images ✅
+// - i need a loop to perform a repetitve task (for the loop)
+// - i need the image source and alt text ✅
+// - i need to store the images data ✅
+// - i need create and add the images to the DOM
+
+let imgURLs = [
+  "https://images.unsplash.com/photo-1444212477490-ca407925329e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZG9nc3xlbnwwfDB8MHx8fDI%3D",
+  "https://images.unsplash.com/photo-1548199973-03cce0bbc87b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZG9nc3xlbnwwfDB8MHx8fDI%3D",
+  "https://images.unsplash.com/photo-1601758176481-e81a6b713126?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8ZG9nc3xlbnwwfDB8MHx8fDI%3D",
+];
+
+let altText = ["A beautiful dog", "A cute puppy", "A lovely dog"];
+
+//select the container
+const imageContainer = document.querySelector(".image-container");
+for (let i = 0; i <= altText.length; i++) {
+  // create the image element
+  const pTag = document.createElement("p"); //<p></p>
+  pTag.textContent = altText[i]; // add the text to the p tag
+  pTag.className = "alt-text"; // add a class to the p tag
+  imageContainer.appendChild(pTag); // add the p tag to the container
+}
