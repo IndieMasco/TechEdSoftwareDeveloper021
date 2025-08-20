59aea5343eebd37544f898bd319dcbca426e8476
sam <s.clarkyy.1994@gmail.com>
2025-08-20 14:53:03 +0100

    break time push

---
+++ week2/callback-ws/app.js
@@ -0,0 +1,139 @@
+console.log("hello world");
+
+//======================================================================
+
+// logs a string to the console to tell the user the function was called
+function notifyUser() {
+  console.log("notifyUser function was called!");
+}
+
+// accepts a function as an argument to run when it has done it's work
+function myAwesomeFunction(notifyUserCallback) {
+  console.log("Running myAwesomeFunction... doing complex tasks...");
+  console.log("Complex task complete. I will notify the user");
+  notifyUserCallback();
+}
+
+// run the awesome function, and pass the notifyUser function to it
+myAwesomeFunction(notifyUser);
+
+//===========================================================================
+
+function myAwesomeFunction(onCompleteCallback) {
+  console.log("Running myAwesomeFunction... doing complex tasks...");
+  console.log("Complex task complete. I will notify the user");
+  onCompleteCallback();
+}
+
+myAwesomeFunction(function () {
+  console.log("notifyUser function was called!");
+});
+
+//====================================================================
+
+function myAwesomeFunction(onSuccessCallback, onFailureCallback) {
+  console.log("Running myAwesomeFunction... doing complex tasks...");
+  console.log("Complex task complete. I will notify the user");
+
+  // randomly choose if success is true or false
+  const success = Math.random() > 0.5;
+  if (success) {
+    onSuccessCallback({
+      message: "This is the message of success",
+    });
+  } else {
+    onFailureCallback();
+  }
+}
+
+function onAwesomeSuccess(data) {
+  console.log("It was successful: ", data.message);
+}
+
+function onAwesomeFailure() {
+  console.log("It failed :(");
+}
+
+myAwesomeFunction(onAwesomeSuccess, onAwesomeFailure);
+
+//============================================================
+
+function add(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function myCalculatorFunction(a, b, operationCallback) {
+  console.log("Doing a calculation using the numbers: ", a, b);
+  console.log(
+    "We might have code that saves the result to a log or a database"
+  );
+
+  return operationCallback(a, b);
+}
+
+const result = myCalculatorFunction(1, 2, add);
+console.log(result); // Output: 3
+
+// const result2 = myCalculatorFunction(2, 3, multiply);
+console.log(result2); // Output: 6
+
+// add the two results together
+const finalResult = myCalculatorFunction(result, result2, add);
+console.log(finalResult); // Output: 9
+
+//========================================================================
+
+function add(a, b, c, d, e, f) {
+  return a + b + c + d + e + f;
+}
+
+function multiply(a, b, c, d, e, f) {
+  return a * b * c * d * e * f;
+}
+
+function myCalculatorFunction(a, b, c, d, e, f, operationCallback) {
+  console.log("Doing a calculation using the numbers: ", a, b, c, d, e, f);
+  console.log(
+    "We might have code that saves the result to a log or a database"
+  );
+
+  return operationCallback(a, b, c, d, e, f);
+}
+
+const result1 = myCalculatorFunction(10, 20, 30, 40, 50, 60, add);
+console.log(result1);
+
+const result2 = myCalculatorFunction(10, 20, 30, 40, 50, 60, multiply);
+console.log(result2);
+
+//=====================================================================
+
+function add(a, b, c) {
+  return a + b + c;
+}
+
+function multiply(a, b, c) {
+  return a * b * c;
+}
+
+function myCalculatorFunction(a, b, c, operationCallback) {
+  console.log("Doing a calculation using the numbers: ", a, b, c);
+  console.log(
+    "We might have code that saves the result to a log or a database"
+  );
+
+  return operationCallback(a, b, c);
+}
+
+// const result1 = myCalculatorFunction(10, 20, 30, add);
+console.log(result1);
+
+// const result2 = myCalculatorFunction(40, 50, 60, multiply);
+console.log(result2);
+
+const result3 = myCalculatorFunction(result1, result2, 1, add);
+console.log(result3);
--- week2/callback/app.js
+++ week2/callback/app.js
@@ -1,1 +1,80 @@
 console.log("hello world!");
+
+// callback function --> used as an argument of a method or another function
+
+const staff = ["sam", "manny", "joe"];
+
+// TODO: add each item in the array to the console ( 3 times --> length)
+// - for loop
+// - forEach() --> array method
+// - for ... of
+
+// when a function does not have a name it is an anonymous funtion
+// we can use anonymous funtions when we so not want to call them again (reuse them)
+// callback function when added inline tend to be anonymous
+
+staff.forEach(function (item) {
+  console.log(item);
+});
+
+//=============================================
+
+// TODO: i want to give the user the foloowing messages: welcome message, their username, missed messages
+// - object --> store users data
+// - function --> one function per task
+
+const jack = {
+  staffName: "jack",
+  userName: "Hollow.Jack",
+  missedMessages: 25,
+};
+
+function welcomeUser(user) {
+  console.log(`welcome ${user}`);
+}
+
+function sayUsername(username) {
+  console.log(`Your username is ${username}`);
+}
+
+function sayMissingMessages(numberOfMessages) {
+  console.log(`You have ${numberOfMessages} missed messages`);
+}
+
+// welcomeUser("sam");
+// sayUsername("sam.clark")
+// sayMissedMessages("7");
+
+//=================================================
+// refactor our code for us to have more control over the three tasks
+
+function enterWebSite(
+  welcome,
+  username,
+  missedMessages,
+  welcomeMessage,
+  userNameData,
+  numberMessage
+) {
+  welcome(welcomeMessage);
+  username(userNameData);
+  missedMessages(numberMessage);
+}
+
+enterWebSite(
+  welcomeUser,
+  sayUsername,
+  sayMissingMessages,
+  "sam",
+  "sam.clark",
+  7
+);
+
+enterWebSite(
+  welcomeUser,
+  sayUsername,
+  sayMissingMessages,
+  jack.staffName,
+  jack.userName,
+  jack.missedMessages
+);
