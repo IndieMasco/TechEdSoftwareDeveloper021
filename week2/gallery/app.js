4b4f47cc5b0ee971ed7a7d0169bb7bab3bab5e16
sam <s.clarkyy.1994@gmail.com>
2025-08-22 12:52:21 +0100

    added more JS in gallery

--- week2/gallery/app.js
+++ week2/gallery/app.js
@@ -1,48 +1,45 @@
-console.log("hello!");
+console.log("hello world");
 
-// TODO: i need to store my image date
-// you can use imahes stored locally or images stored remotley (unsplash)
+//TODO: I need to store my images data
+//You can use images stored locally or images stored remotely (unsplash...)
 
 const images = [
   {
-    url: "url1",
-    alttext: text1,
+    url: "url1", //relative path for local images OR link to the image
+    altText: "altText1",
   },
   {
     url: "url2",
-    alttext: text2,
+    altText: "altText2",
   },
   {
     url: "url3",
-    alttext: text3,
+    altText: "altText3",
   },
 ];
-
-// TODO: i need to create my thumnail images
-
-// function createThimbnauls(){
+//TODO: I need to create my thumbnail images
+// function createThumbnails(){
 // select the DOM element (thumbnail-container) to contain our thumbnails
-// this is a repetitve task --> loop through our array (using the length property)(image.length)
-
-// inside are loop we need to do this:
-// - create img ekement
-// - update the src and alt attributes of the img elements to match those in the array (HINT - parameters)
-// - give each img a className (HINT - append the created images to the thumbnail-container)
-// - add an event lister to each image --> the event handler of this listerner is the function you write to create large images
-// - append the create images to the thumbnail-containter
+//this is a repetitive task --> loop through our array (using the length property)
+//Inside our loop we need to do this:
+// - create img element
+// - update the src and alt attributes of the img element to match those in the array (parameters)
+// - give each img a className (img.className)
+// - add an event listener to each image --> the event handler of this listener is the function you write to create large images
+// - append the created images to the thumbnail-container
 // }
 
-// TODO: i neeed to create large images
-// this task is the event handler for the thumbnail events
+//TODO: I need to create my large images
+//this task is the event handler for the thumbnail events
 // function createLargeImagesHandler(){
-// select the large-image-container
-// delete the current image in the large-image-container
-// largeImageContainer.innerHTML = null or ""
+//select the large-image-container
+//delete the current image in the large-image-container
+// largeImageContainer.innerHTML = null OR ""
 // create an image
 // update the src and alt values
-// ass a className for styling
-// append the img to the container
-//}
+// add a className for styling
+//append the img to the container
+// }
 
-// add this event handler to the thumbnail event
-// you call the createThumbnailfunction
+//add this event handler to the thumbanil event
+//you call the createThumbanils function
