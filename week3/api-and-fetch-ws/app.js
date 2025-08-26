b4bcca032a9b5de02584e5574e796badfa793540
sam <s.clarkyy.1994@gmail.com>
2025-08-26 16:47:23 +0100

    finished workshop

---
+++ week3/api-and-fetch-ws/app.js
@@ -0,0 +1,33 @@
+console.log("hello!");
+
+// async function getMyStuffFromOverThere() {
+//   const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+//   console.log("HTTP Response:", response);
+//   const json = await response.json();
+//   console.log("JSON Data:", json);
+// }
+
+// getMyStuffFromOverThere();
+
+//==============================================================================
+
+async function repo1() {
+  const response = await fetch(
+    "https://api.github.com/repos/IndieMasco/TechEdSoftwareDeveloper021"
+  );
+  //   console.log("HTTP Response:", response);
+  const json = await response.json();
+  console.log("JSON Data:", json);
+  return json.stargazers_count;
+}
+
+repo1();
+
+async function repo2() {
+  const starCount = await repo1();
+  const textBox = document.createElement("p");
+  textBox.textContent = starCount;
+  document.body.appendChild(textBox);
+}
+
+repo2();
