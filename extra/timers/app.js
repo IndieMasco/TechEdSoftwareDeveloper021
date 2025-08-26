b8f4290de7baf2630252a1b5889a847688b72a62
sam <s.clarkyy.1994@gmail.com>
2025-08-26 21:31:58 +0100

    playing around with timers

---
+++ extra/timers/app.js
@@ -0,0 +1,95 @@
+console.log("hello!");
+
+setTimeout(() => {
+  console.log("Delayed for 1 second.");
+}, 1000);
+
+//====================================================
+
+setTimeout(() => {
+  console.log("this is the first message");
+}, 5000);
+setTimeout(() => {
+  console.log("this is the second message");
+}, 3000);
+setTimeout(() => {
+  console.log("this is the third message");
+}, 1000);
+
+// Output:
+// this is the third message
+// this is the second message
+// this is the first message
+
+//====================================================
+
+const myArray = ["zero", "one", "two"];
+myArray.myMethod = function (sProperty) {
+  console.log(arguments.length > 0 ? this[sProperty] : this);
+};
+
+myArray.myMethod(); // prints "zero,one,two"
+myArray.myMethod(1); // prints "one"
+
+//====================================================
+
+setTimeout(function () {
+  myArray.myMethod();
+}, 2.0 * 1000); // prints "zero,one,two" after 2 seconds
+setTimeout(function () {
+  myArray.myMethod("1");
+}, 2.5 * 1000); // prints "one" after 2.5 seconds
+
+//====================================================
+
+const myArray = ["zero", "one", "two"];
+const myBoundMethod = function (sProperty) {
+  console.log(arguments.length > 0 ? this[sProperty] : this);
+}.bind(myArray);
+
+myBoundMethod(); // prints "zero,one,two" because 'this' is bound to myArray in the function
+myBoundMethod(1); // prints "one"
+setTimeout(myBoundMethod, 1.0 * 1000); // still prints "zero,one,two" after 1 second because of the binding
+setTimeout(myBoundMethod, 1.5 * 1000, "1"); // prints "one" after 1.5 seconds
+
+//====================================================
+
+let last = 0;
+let iterations = 10;
+
+function timeout() {
+  // log the time of this call
+  log(new Date().getMilliseconds());
+  // if we are not finished, schedule the next call
+  if (iterations-- > 0) {
+    setTimeout(timeout, 0);
+  }
+}
+
+function run() {
+  // clear the log
+  const log = document.querySelector("#log");
+  while (log.lastElementChild) {
+    log.removeChild(log.lastElementChild);
+  }
+
+  // initialize iteration count and the starting timestamp
+  iterations = 10;
+  last = new Date().getMilliseconds();
+  // start timer
+  setTimeout(timeout, 0);
+}
+
+function log(now) {
+  // log the last timestamp, the new timestamp, and the difference
+  const tableBody = document.getElementById("log");
+  const logRow = tableBody.insertRow();
+  logRow.insertCell().textContent = last;
+  logRow.insertCell().textContent = now;
+  logRow.insertCell().textContent = now - last;
+  last = now;
+}
+
+document.querySelector("#run").addEventListener("click", run);
+
+//====================================================
