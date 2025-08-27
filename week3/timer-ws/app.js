5963c70bbbb5be71f8f3e75117a6cf2ed7017efc
sam <s.clarkyy.1994@gmail.com>
2025-08-27 12:52:17 +0100

    finished workshop

--- week3/timer-ws/app.js
+++ week3/timer-ws/app.js
@@ -1,1 +1,18 @@
 console.log("HELLO!");
+
+// TODO: CLick a button to show text for 5 seconds
+
+// linking HTML with my JS
+const button = document.getElementById("button");
+const text = document.getElementById("text");
+
+// Add event listener
+// Make text visible
+button.addEventListener("click", () => {
+  text.style.visibility = "visible";
+
+  // Hiding text
+  setTimeout(() => {
+    text.style.visibility = "hidden";
+  }, 5000);
+});
