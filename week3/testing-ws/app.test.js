6dce6c93b064bc5e3231b9c47c1bc394aa13da78
sam <s.clarkyy.1994@gmail.com>
2025-08-28 15:10:26 +0100

    finished workshop

--- week3/testing-ws/app.test.js
+++ week3/testing-ws/app.test.js
@@ -1,1 +1,44 @@
+import { describe, expect, test } from "vitest";
 import { findMaxValue, factorial, areArraysEqual, toTitleCase } from "./app";
+
+describe("find max value", function () {
+  test("array max value", function () {
+    const result = findMaxValue();
+    const expectedResult = null;
+
+    expect(result).toBe(expectedResult);
+  });
+});
+
+//===================================================
+
+describe("find factorial", function () {
+  test("find factorial", function () {
+    const result = factorial(5);
+    const expectedResult = 120;
+
+    expect(result).toBe(expectedResult);
+  });
+});
+
+//===================================================
+
+describe("find array equal", function () {
+  test("find array equal", function () {
+    const result = areArraysEqual(3, 2, 1);
+    const expectedResult = false;
+
+    expect(result).toBe(expectedResult);
+  });
+});
+
+//===================================================
+
+describe("find to tile case", function () {
+  test("find to tile case", function () {
+    const result = toTitleCase(12345);
+    const expectedResult = "";
+
+    expect(result).toBe(expectedResult);
+  });
+});
