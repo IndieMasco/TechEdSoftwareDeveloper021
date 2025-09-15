869258407080cdac252f7afc94bb793540f26639
sam <s.clarkyy.1994@gmail.com>
2025-09-15 15:47:56 +0100

    small edit

--- week6/react-components/src/App.jsx
+++ week6/react-components/src/App.jsx
@@ -15,12 +15,30 @@ export default function app() {
     // HTML elements or nested components
     // Fragment --> it's an empty HTML tag that we use for nesting purposes
     <>
-      {/* We nest components as self-closing elements  */}
+      {/* We nest components as self-closing elements */}
       <Header />
       <h1>Welcome!</h1>
-      <MainSection />
-      <MainSection />
-      <MainSection />
+      <main>
+        {/* We add props in the samw ay as we did attribu */}
+        <MainSection
+          source="https://images.unsplash.com/photo-1551189014-fe516aed0e9e?q=80&w=886&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          altText="frog1"
+          content="content1"
+          link="https://unsplash.com/"
+        />
+        <MainSection
+          source="https://images.unsplash.com/photo-1518737496070-5bab26f59b3f?q=80&w=924&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          altText="frog2"
+          content="content2"
+          link="https://unsplash.com/"
+        />
+        <MainSection
+          source="https://images.unsplash.com/photo-1642250602588-3167b4e0c6e1?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          altText="frog3"
+          content="content3"
+          link="https://unsplash.com/"
+        />
+      </main>
       <Footer />
     </>
   );
--- week6/react-components/src/components/MainSection.jsx
+++ week6/react-components/src/components/MainSection.jsx
@@ -1,9 +1,32 @@
-export default function MainSection() {
+// Props parameter represents the props we added in the component instance (in this case, in app.jsx)
+
+export default function MainSection(props) {
   return (
     <section>
-      <img src="#" alt="image1" />
-      <p>this is just some text to show on the page</p>
-      <button>CLICK ME!</button>
+      <img src={props.source} alt="props.altText" />
+      <p>{props.content}</p>
+      <button>
+        <a href={props.link}>CLICK ME!</a>
+      </button>
     </section>
   );
 }
+
+// props {
+//   source: "",
+//   altText: "",
+//   content: "",
+//   link: "",
+// }
+
+// export default function MainSection({source, altText, content, link}) {
+//   return (
+//     <section>
+//       <img src={source} alt={altText} />
+//       <p>{content}</p>
+//       <button>
+//         <a href={link}>CLICK ME!</a>
+//       </button>
+//     </section>
+//   );
+// }
