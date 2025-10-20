ab1adba8a8f95fa9844b27ae5ef944524b1927ec
sam <s.clarkyy.1994@gmail.com>
2025-10-20 11:12:34 +0100

    finished demo

--- week-11/react-context/client/src/App.jsx
+++ week-11/react-context/client/src/App.jsx
@@ -1,9 +1,16 @@
-import "./App.css";
+import Button from "./components/Button";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { CountProvider } from "./context/countContext";
 
 export default function App() {
   return (
-    <>
-      <h1>HELLO!</h1>
-    </>
+    <CountProvider>
+      <div>
+        <Header />
+        <Button />
+        <Footer />
+      </div>
+    </CountProvider>
   );
 }
